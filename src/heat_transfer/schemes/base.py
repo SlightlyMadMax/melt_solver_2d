c247ac2db9fc9fcdbc6ec1a961b020e87866ee93
SlightlyMadMax <makcandreev2001@mail.ru>
2024-12-21 21:23:21 +0300

    refactoring

--- src/heat_transfer/schemes/base.py
+++ src/heat_transfer/schemes/base.py
@@ -2,8 +2,6 @@ from abc import abstractmethod
 import numpy as np
 from numpy.typing import NDArray
 
-from src.boundary_conditions import BoundaryCondition
-from src.geometry import DomainGeometry
 from src.solver import Sweep2DScheme
 from src.heat_transfer.parameters import ThermalParameters
 
@@ -11,25 +9,17 @@ from src.heat_transfer.parameters import ThermalParameters
 class HeatTransferScheme(Sweep2DScheme):
     def __init__(
         self,
-        geometry: DomainGeometry,
         parameters: ThermalParameters,
-        top_bc: BoundaryCondition,
-        right_bc: BoundaryCondition,
-        bottom_bc: BoundaryCondition,
-        left_bc: BoundaryCondition,
         fixed_delta: bool = False,
+        *args,
+        **kwargs,
     ):
-        super().__init__(
-            geometry=geometry,
-            top_bc=top_bc,
-            right_bc=right_bc,
-            bottom_bc=bottom_bc,
-            left_bc=left_bc,
-        )
-        self.fixed_delta = fixed_delta
-        self.parameters = parameters
+        super().__init__(*args, **kwargs)
 
-        # Pre-allocate arrays, used for calculations
+        self.parameters = parameters
+        self.fixed_delta = fixed_delta
+
+        # Pre-allocate some arrays that will be used for calculations
         self._temp_u: NDArray[np.float64] = np.empty(
             (self.geometry.n_y, self.geometry.n_x)
         )
--- src/solver.py
+++ src/solver.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 import numba
 import numpy as np
 from numpy.typing import NDArray
@@ -5,6 +7,13 @@ from abc import ABC, abstractmethod
 
 from src.boundary_conditions import BoundaryCondition
 from src.geometry import DomainGeometry
+
+
+class SchemeType(Enum):
+    EXPLICIT = 1, "Explicit"
+    IMPLICIT = 2, "Implicit"
+    SEMI_IMPLICIT = 3, "Semi-implicit"
+    LINEARIZED_IMPLICIT = 4, "Linearized implicit"
 
 
 class BaseScheme(ABC):
@@ -23,25 +32,14 @@ class BaseScheme(ABC):
         self.left_bc = left_bc
 
     @abstractmethod
-    def solve(self, *kwargs) -> NDArray[np.float64]: ...
+    def solve(self, *args, **kwargs) -> NDArray[np.float64]: ...
 
 
 class Sweep2DScheme(BaseScheme, ABC):
-    def __init__(
-        self,
-        geometry: DomainGeometry,
-        top_bc: BoundaryCondition,
-        right_bc: BoundaryCondition,
-        bottom_bc: BoundaryCondition,
-        left_bc: BoundaryCondition,
-    ):
-        super().__init__(
-            geometry=geometry,
-            top_bc=top_bc,
-            right_bc=right_bc,
-            bottom_bc=bottom_bc,
-            left_bc=left_bc,
-        )
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Pre-allocate some arrays that will be used for calculations
         self._a_x: NDArray[np.float64] = np.empty((self.geometry.n_x - 1))
         self._b_x: NDArray[np.float64] = np.empty((self.geometry.n_x - 1))
         self._c_x: NDArray[np.float64] = np.empty((self.geometry.n_x - 1))
@@ -52,9 +50,9 @@ class Sweep2DScheme(BaseScheme, ABC):
     @staticmethod
     @abstractmethod
     @numba.jit(nopython=True)
-    def _compute_sweep_x(*kwargs) -> NDArray[np.float64]: ...
+    def _compute_sweep_x(*args, **kwargs) -> NDArray[np.float64]: ...
 
     @staticmethod
     @abstractmethod
     @numba.jit(nopython=True)
-    def _compute_sweep_y(*kwargs) -> NDArray[np.float64]: ...
+    def _compute_sweep_y(*args, **kwargs) -> NDArray[np.float64]: ...
