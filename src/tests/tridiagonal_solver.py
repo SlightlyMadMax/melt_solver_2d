92470247f4b84b8d699e9abd5957440d32702344
SlightlyMadMax <makcandreev2001@mail.ru>
2024-12-22 23:21:34 +0300

    add tridiagonal solver test

---
+++ src/tests/tridiagonal_solver.py
@@ -0,0 +1,34 @@
+import numpy as np
+from numpy.testing import assert_almost_equal
+
+from src.utils import solve_tridiagonal
+
+
+def test_solve_tridiagonal():
+    n = 5
+    a = -1 * np.ones(n - 1, dtype=np.float64)
+    b = 2 * np.ones(n, dtype=np.float64)
+    c = -1 * np.ones(n - 1, dtype=np.float64)
+    f = np.array([1, 1, 1, 1, 1], dtype=np.float64)
+
+    # Dirichlet boundary conditions
+    left_type = 1
+    right_type = 1
+    left_value = 2.5
+    right_value = 2.5
+
+    u = solve_tridiagonal(a, b, c, f, left_type, right_type, left_value, right_value)
+
+    A = np.zeros((n, n), dtype=np.float64)
+    np.fill_diagonal(A, b)
+    np.fill_diagonal(A[1:], a)
+    np.fill_diagonal(A[:, 1:], c)
+
+    expected_u = np.linalg.solve(A, f)
+
+    assert_almost_equal(u, expected_u, decimal=6)
+    print("Test passed!")
+
+
+test_solve_tridiagonal()
+
