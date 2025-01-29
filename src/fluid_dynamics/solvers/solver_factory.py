fix sign in poisson equation solvers

src/fluid_dynamics/solvers/solver_factory.py
@@ -130,5 +130,5 @@ class NavierStokesSolver:
         time: float,
     ) -> np.ndarray:
         return self.stream_function_solver.solve(
-            initial_guess=initial_guess, rhs=vorticity, time=time
+            initial_guess=initial_guess, rhs=-vorticity, time=time
         )

src/fluid_dynamics/solvers/stream_function_solvers/matrix_sweep.py
@@ -118,7 +118,7 @@ class MatrixSweepPoissonSolver(BaseSolver):
         """
         self._result = np.copy(initial_guess)
         self._matrix_sweep(
-            f=-rhs,
+            f=rhs,
             right_value=(
                 self.right_bc.get_value(t=time)
                 if self.right_bc.boundary_type == BoundaryConditionType.DIRICHLET

src/tests/test_sor.py
@@ -56,7 +56,7 @@ def test_sor_poisson_solver():
     initial_guess = np.zeros((geometry.n_y, geometry.n_x), dtype=np.float64)
 
     start_time = time.perf_counter()
-    result = solver.solve(initial_guess=initial_guess, rhs=-rhs, time=0.0)
+    result = solver.solve(initial_guess=initial_guess, rhs=rhs, time=0.0)
     print(f"Elapsed time: {time.perf_counter() - start_time:.2f} s.")
 
     error = np.linalg.norm(result - analytical_solution, ord=2)