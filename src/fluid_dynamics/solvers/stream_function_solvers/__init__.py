e5a9601e4b64f2882c8546229ff9c06f62a72887
SlightlyMadMax <makcandreev2001@mail.ru>
2025-02-01 15:43:04 +0300

    fix cg not being registered

--- src/fluid_dynamics/solvers/stream_function_solvers/__init__.py
+++ src/fluid_dynamics/solvers/stream_function_solvers/__init__.py
@@ -1,3 +1,4 @@
 from .sor import SORPoissonSolver
 from .matrix_sweep import MatrixSweepPoissonSolver
+from .cg import ConjugateGradientSolver
 from .registry import StreamFunctionSolverName, StreamFunctionSolverRegistry
