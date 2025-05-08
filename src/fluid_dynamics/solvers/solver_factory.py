58f7682e6872051629f30e588e057136b9070c2b
SlightlyMadMax <makcandreev2001@mail.ru>
2025-05-08 17:24:34 +0300

    signs

--- src/fluid_dynamics/solvers/solver_factory.py
+++ src/fluid_dynamics/solvers/solver_factory.py
@@ -290,11 +290,11 @@ def construct_rhs_for_cg(
         for j in range(1, geometry.n_y - 1):
             b[j, i] = -vorticity[j, i] - 0.5 * tau * (
                 (c_ind[j, i] + rho[j, i] / parameters.reynolds_number) * sf_nm1[j, i]
-                - (
+                + (
                     conv_x[j, i, 0] * sf_nm1[j, i + 1]
                     + conv_x[j, i, 2] * sf_nm1[j, i - 1]
                 )
-                - (
+                + (
                     conv_y[j, i, 0] * sf_nm1[j + 1, i]
                     + conv_y[j, i, 2] * sf_nm1[j - 1, i]
                 )
