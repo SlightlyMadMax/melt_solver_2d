a21eeca8d81e3e771b5c883d3d4523a0c5ec051c
SlightlyMadMax <makcandreev2001@mail.ru>
2025-05-14 01:18:56 +0300

    update latent_heat_density_field

--- src/utils/plot_latent_heat.py
+++ src/utils/plot_latent_heat.py
@@ -2,22 +2,21 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 from src.core.geometry import DomainGeometry
+from src.heat_transfer.coefficient_smoothing.coefficients import delta_gauss
+from src.heat_transfer.coefficient_smoothing.mushy_zone import get_dilated_mushy_mask
 from src.heat_transfer.pt_boundary import get_phase_trans_boundary
 
 
 def latent_heat_density_field(
     u: np.ndarray, u_pt: float, delta: np.ndarray, l_solid: float
 ) -> np.ndarray:
+    n_y, n_x = u.shape
     val = np.zeros_like(u)
-    mask = np.abs(u - u_pt) < delta
-
-    u_masked = u[mask]
-    delta_masked = delta[mask]
-    diff = u_masked - u_pt
-    denom = (delta_masked**2 - diff**2) ** 1.5
-    sech_sq = 1 / np.cosh(3.0 * diff / np.sqrt(delta_masked**2 - diff**2)) ** 2
-
-    val[mask] = 1.5 * delta_masked**2 / denom * sech_sq
+    mushy_mask = get_dilated_mushy_mask(u_dim=u, u_pt=u_pt, delta=delta, extend_by=1)
+    for j in range(n_y):
+        for i in range(n_x):
+            if mushy_mask[j, i]:
+                val[j, i] = delta_gauss(u[j, i], u_pt, delta[j, i])
     return l_solid * val
 
 
@@ -57,9 +56,9 @@ def plot_latent_heat_field(
     cbar = plt.colorbar(contour, label="Latent Heat Source")
 
     for i in range(X.shape[0]):
-        plt.plot(X[i, :], Y[i, :], color='white', linewidth=0.3, alpha=0.6)
+        plt.plot(X[i, :], Y[i, :], color="white", linewidth=0.3, alpha=0.6)
     for j in range(X.shape[1]):
-        plt.plot(X[:, j], Y[:, j], color='white', linewidth=0.3, alpha=0.6)
+        plt.plot(X[:, j], Y[:, j], color="white", linewidth=0.3, alpha=0.6)
 
     plt.scatter(X_b, Y_b, s=1, linewidths=0.1, color="r", label="Граница ф.п.")
     ax.legend()
