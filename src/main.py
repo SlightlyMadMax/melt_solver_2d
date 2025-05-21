60bf45d1f041778556e0d478385cab0dbcffb99b
SlightlyMadMax <makcandreev2001@mail.ru>
2025-05-21 14:06:46 +0300

    ref

--- src/main.py
+++ src/main.py
@@ -84,19 +84,19 @@ if __name__ == "__main__":
         f"Mushy zone width for the initial temperature distribution: {init_delta:.2f}"
     )
 
-    # plot_temperature(
-    #     u=u * thermal_params.delta_u + thermal_params.u_ref,
-    #     u_pt=thermal_params.u_pt,
-    #     geometry=geometry,
-    #     time=0.0,
-    #     graph_id=0,
-    #     plot_boundary=True,
-    #     show_graph=True,
-    #     min_temp=min_temp + ABS_ZERO,
-    #     max_temp=max_temp + ABS_ZERO,
-    #     actual_temp_units=TemperatureUnit.KELVIN,
-    #     display_temp_units=TemperatureUnit.CELSIUS,
-    # )
+    plot_temperature(
+        u=u * thermal_params.delta_u + thermal_params.u_ref,
+        u_pt=thermal_params.u_pt,
+        geometry=geometry,
+        time=0.0,
+        graph_id=0,
+        plot_boundary=True,
+        show_graph=True,
+        min_temp=min_temp + ABS_ZERO,
+        max_temp=max_temp + ABS_ZERO,
+        actual_temp_units=TemperatureUnit.KELVIN,
+        display_temp_units=TemperatureUnit.CELSIUS,
+    )
 
     # Temperature boundary conditions
     u_bcs = BoundaryConditions(
