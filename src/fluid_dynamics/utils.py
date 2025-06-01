14d2b9aabd9906890ebca1cba287a75c08875634
SlightlyMadMax <makcandreev2001@mail.ru>
2025-06-01 22:31:41 +0300

    add calculate_velocity_from_sf utility function

--- src/fluid_dynamics/utils.py
+++ src/fluid_dynamics/utils.py
@@ -38,9 +38,7 @@ def calculate_indicator_function(
     # result[interior] = result_interior
 
     # --- Variant 2: error‐function form -------------------
-    result[interior] = (
-        0.5 * inv_eps2 * (1.0 - erf(diff_u / (np.sqrt(2.0) * delta)))
-    )
+    result[interior] = 0.5 * inv_eps2 * (1.0 - erf(diff_u / (np.sqrt(2.0) * delta)))
 
     # --- Variant 3: hyperbolic‐tangent form ---------------
     # result[interior] = (
@@ -81,6 +79,24 @@ def calculate_vorticity_from_sf(
                 -(sf[j + 1, i] - 2 * sf[j, i] + sf[j - 1, i]) * inv_dy2
                 - (sf[j, i + 1] - 2 * sf[j, i] + sf[j, i - 1]) * inv_dx2
             )
+
+
+@njit
+def calculate_velocity_from_sf(
+    sf: NDArray[np.float64],
+    v_x: NDArray[np.float64],
+    v_y: NDArray[np.float64],
+    dx: float,
+    dy: float,
+):
+    inv_2dx = 1.0 / (2.0 * dx)
+    inv_2dy = 1.0 / (2.0 * dy)
+
+    v_x[:, :] = 0.0
+    v_y[:, :] = 0.0
+
+    v_x[1:-1, 1:-1] = inv_2dy * (sf[2:, 1:-1] - sf[:-2, 1:-1])
+    v_y[1:-1, 1:-1] = -inv_2dx * (sf[1:-1, 2:] - sf[1:-1, :-2])
 
 
 class VorticityBCMixin:
