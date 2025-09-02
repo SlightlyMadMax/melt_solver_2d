edbce6cf7c681d74c2eb61aa4e57e679452ec030
SlightlyMadMax <makcandreev2001@mail.ru>
2025-09-02 18:30:56 +0300

    fix not recalculating delta

--- src/main.py
+++ src/main.py
@@ -158,8 +158,9 @@ if __name__ == "__main__":
     start_time = time.perf_counter()
     for n in range(1, geometry.n_t):
         t = n * geometry.dt
-        delta = get_mushy_zone_temperature_range(u, cfg.u_pt_nd, n_nodes=2)
+        delta = get_mushy_zone_temperature_range(u, cfg.u_pt_nd, n_nodes=3)
         u = heat_transfer_solver.solve(u=u, sf=sf, delta=delta, time=t)
+        delta = get_mushy_zone_temperature_range(u, cfg.u_pt_nd, n_nodes=3)
         sf, w = navier_solver.solve(w=w, sf=sf, u=u, delta=max(delta), time=t)
         if n % cfg.save_interval == 0:
             u_dim = u * delta_u + u_ref
