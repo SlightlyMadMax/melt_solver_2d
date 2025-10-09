43820b88a4e25462fe272b1a151592bd70453ab6
SlightlyMadMax <makcandreev2001@mail.ru>
2025-10-09 22:31:36 +0300

    update plot_water_freezing.py

--- src/utils/plot_water_freezing.py
+++ src/utils/plot_water_freezing.py
@@ -3,10 +3,13 @@ import matplotlib.pyplot as plt
 import matplotlib.lines as mlines
 
 from src.core.geometry import DomainGeometry
+from src.fluid_dynamics.init_values import initialize_velocity
+from src.fluid_dynamics.plotting import plot_velocity_field
+from src.fluid_dynamics.utils import calculate_velocity_from_sf
 from src.heat_transfer.pt_boundary import get_phase_trans_boundary
 from src.parameters.config import ExperimentConfig
 
-img = plt.imread("../../data/kowalewski.png")
+# img = plt.imread("../../data/kowalewski.png")
 
 cfg: ExperimentConfig = ExperimentConfig.load_from_file(
     "../../parameter_sets/water/freezing.json"
@@ -15,23 +18,44 @@ geometry: DomainGeometry = cfg.geometry
 
 min_temp = 263.15
 max_temp = 283.15
+delta_u = cfg.delta_u
+u_ref = cfg.u_ref
+u_pt = cfg.material_props.u_pt
+l = cfg.l
+v = cfg.v
 
-fig, ax = plt.subplots()
-ax.imshow(img, extent=[0, geometry.width, 0, geometry.height])
+# fig, ax = plt.subplots()
+# ax.imshow(img, extent=[0, geometry.width, 0, geometry.height])
 
 data = np.load("../../data/water_freezing/after_freezing.npz")
 u = data["u"]
-X_b, Y_b = get_phase_trans_boundary(cfg=cfg, u=u * cfg.delta_u + cfg.u_ref)
-ax.plot(X_b, Y_b, linestyle="--", color="red", linewidth=2)
-
-legend_elements = [
-    mlines.Line2D(
-        [], [], linestyle="--", color="red", linewidth=2, label="Численное решение"
-    ),
-]
-ax.legend(handles=legend_elements, fontsize=12)
-
-ax.set_xlabel("X", fontsize=14)
-ax.set_ylabel("Y", fontsize=14)
-plt.tight_layout()
-plt.show()
+sf = data["sf"]
+w = data["w"]
+u_dim = u * delta_u + u_ref
+sf_dim = sf * v * l
+v_x, v_y = initialize_velocity(geom=geometry)
+calculate_velocity_from_sf(sf_dim, v_x, v_y, geometry.dx, geometry.dy)
+plot_velocity_field(
+    v_x,
+    v_y,
+    u_dim,
+    cfg,
+    0,
+    True,
+    True,
+    stride=8,
+)
+# X_b, Y_b = get_phase_trans_boundary(cfg=cfg, u=u * cfg.delta_u + cfg.u_ref)
+# ax.plot(X_b, Y_b, linestyle="--", color="red", linewidth=2)
+#
+# legend_elements = [
+#     mlines.Line2D(
+#         [], [], linestyle="--", color="red", linewidth=2, label="Численное решение"
+#     ),
+# ]
+# ax.legend(handles=legend_elements, fontsize=12)
+#
+# ax.set_xlabel("X", fontsize=14)
+# ax.set_ylabel("Y", fontsize=14)
+# plt.tight_layout()
+# plt.show()
