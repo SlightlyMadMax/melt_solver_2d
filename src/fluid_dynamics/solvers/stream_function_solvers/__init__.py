ff08016e42ca85c0a6beb555d89ef21e9a302261
SlightlyMadMax <makcandreev2001@mail.ru>
2025-10-17 20:43:32 +0300

    add cg gpu solver

--- src/fluid_dynamics/solvers/stream_function_solvers/__init__.py
+++ src/fluid_dynamics/solvers/stream_function_solvers/__init__.py
@@ -1,5 +1,6 @@
 from .sor import SORPoissonSolver
 from .matrix_sweep import MatrixSweepPoissonSolver
 from .cg import ConjugateGradientSolver
+from .cg_gpu import ConjugateGradientGPUSolver
 from .amg import AlgebraicMultigridSolver
 from .registry import StreamFunctionSolverName, StreamFunctionSolverRegistry
