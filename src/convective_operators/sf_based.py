72f116941d5dcbb1f65b94a374616e1cbe870b9c
SlightlyMadMax <makcandreev2001@mail.ru>
2025-10-29 18:20:10 +0300

    make StreamFunctionBasedConvectiveOperator methods non-static

--- src/convective_operators/sf_based.py
+++ src/convective_operators/sf_based.py
@@ -44,59 +44,18 @@ class StreamFunctionBasedConvectiveOperator(BaseConvectiveOperator):
         conv_x[:] = 0.0
         conv_y[:] = 0.0
 
-        self.compute_velocity_from_sf(
-            sf=sf,
-            v_x=self._v_x,
-            v_y=self._v_y,
-            dx=dx,
-            dy=dy,
-        )
+        self.compute_velocity_from_sf(sf=sf)
 
         if self.form == ConvectiveTermForm.UPWIND:
-            self._compute_upwind_components(
-                v_x=self._v_x,
-                v_y=self._v_y,
-                result_x=conv_x,
-                result_y=conv_y,
-                dx=dx,
-                dy=dy,
-            )
+            self._compute_upwind_components(result_x=conv_x, result_y=conv_y)
         elif self.form == ConvectiveTermForm.DIVERGENT_CENTRAL:
-            self._compute_div_components(
-                v_x=self._v_x,
-                v_y=self._v_y,
-                result_x=conv_x,
-                result_y=conv_y,
-                dx=dx,
-                dy=dy,
-            )
+            self._compute_div_components(result_x=conv_x, result_y=conv_y)
         elif self.form == ConvectiveTermForm.NON_DIVERGENT_CENTRAL:
-            self._compute_non_div_components(
-                v_x=self._v_x,
-                v_y=self._v_y,
-                result_x=conv_x,
-                result_y=conv_y,
-                dx=dx,
-                dy=dy,
-            )
+            self._compute_non_div_components(result_x=conv_x, result_y=conv_y)
         elif self.form == ConvectiveTermForm.SYMMETRIC:
-            self._compute_div_components(
-                v_x=self._v_x,
-                v_y=self._v_y,
-                result_x=conv_x,
-                result_y=conv_y,
-                dx=dx,
-                dy=dy,
-            )
+            self._compute_div_components(result_x=conv_x, result_y=conv_y)
             temp_x, temp_y = np.copy(conv_x), np.copy(conv_y)
-            self._compute_non_div_components(
-                v_x=self._v_x,
-                v_y=self._v_y,
-                result_x=conv_x,
-                result_y=conv_y,
-                dx=dx,
-                dy=dy,
-            )
+            self._compute_non_div_components(result_x=conv_x, result_y=conv_y)
             conv_x[:] = 0.5 * (temp_x + conv_x)
             conv_y[:] = 0.5 * (temp_y + conv_y)
         else:
@@ -105,32 +64,9 @@ class StreamFunctionBasedConvectiveOperator(BaseConvectiveOperator):
         if u is not None and u_pt is not None:
             self._restrict(conv_x=conv_x, conv_y=conv_y, u=u, u_pt=u_pt)
 
-    @staticmethod
-    def compute_velocity_from_sf(
-        sf: NDArray[np.float64],
-        v_x: NDArray[np.float64],
-        v_y: NDArray[np.float64],
-        dx: float,
-        dy: float,
-    ) -> None:
-        """
-        Compute the velocity components (v_x and v_y) from the stream function.
-
-        This function calculates the velocity field using the relationship between
-        the stream function and velocity components in 2D incompressible flow:
-            v_x =  d(stream function) / dy
-            v_y = -d(stream function) / dx
-
-        The velocity is computed using central differences for interior points.
-        At the boundary points, the velocity components are set to zero to enforce the no-slip boundary condition.
-        :param sf: A 2D numpy array representing the stream function values
-                   over the computational grid.
-        :param v_x: A 2D array where the x-component of the velocity field will be stored.
-        :param v_y: A 2D array where the y-component of the velocity field will be stored.
-        :param dx: Grid spacing in the x-direction.
-        :param dy: Grid spacing in the y-direction.
-        :return: None
-        """
+    def compute_velocity_from_sf(self, sf: NDArray[np.float64]) -> None:
+        dx, dy, _ = self.cfg.scaled_grid_steps
+        v_x, v_y = self._v_x, self._v_y
         inv_2dx = 1.0 / (2.0 * dx)
         inv_2dy = 1.0 / (2.0 * dy)
 
@@ -140,15 +76,11 @@ class StreamFunctionBasedConvectiveOperator(BaseConvectiveOperator):
         v_x[1:-1, 1:-1] = inv_2dy * (sf[2:, 1:-1] - sf[:-2, 1:-1])
         v_y[1:-1, 1:-1] = -inv_2dx * (sf[1:-1, 2:] - sf[1:-1, :-2])
 
-    @staticmethod
     def _compute_upwind_components(
-        v_x: NDArray[np.float64],
-        v_y: NDArray[np.float64],
-        result_x: NDArray[np.float64],
-        result_y: NDArray[np.float64],
-        dx: float,
-        dy: float,
+        self, result_x: NDArray[np.float64], result_y: NDArray[np.float64]
     ) -> None:
+        dx, dy, _ = self.cfg.scaled_grid_steps
+        v_x, v_y = self._v_x, self._v_y
         inv_2dx = 1.0 / (2.0 * dx)
         inv_2dy = 1.0 / (2.0 * dy)
 
@@ -176,15 +108,11 @@ class StreamFunctionBasedConvectiveOperator(BaseConvectiveOperator):
         result_y[jm, im, 1] = inv_2dy * ((vyp + np.abs(vyp)) - (vym - np.abs(vym)))
         result_y[jm, im, 2] = -inv_2dy * (vym + np.abs(vym))
 
-    @staticmethod
     def _compute_div_components(
-        v_x: NDArray[np.float64],
-        v_y: NDArray[np.float64],
-        result_x: NDArray[np.float64],
-        result_y: NDArray[np.float64],
-        dx: float,
-        dy: float,
+        self, result_x: NDArray[np.float64], result_y: NDArray[np.float64]
     ) -> None:
+        dx, dy, _ = self.cfg.scaled_grid_steps
+        v_x, v_y = self._v_x, self._v_y
         inv_2dx = 1.0 / (2.0 * dx)
         inv_2dy = 1.0 / (2.0 * dy)
 
@@ -200,15 +128,11 @@ class StreamFunctionBasedConvectiveOperator(BaseConvectiveOperator):
         result_y[jm, im, 1] = 0.0
         result_y[jm, im, 2] = -inv_2dy * v_y[jm2, im]  # -v_y[j-1, i]
 
-    @staticmethod
     def _compute_non_div_components(
-        v_x: NDArray[np.float64],
-        v_y: NDArray[np.float64],
-        result_x: NDArray[np.float64],
-        result_y: NDArray[np.float64],
-        dx: float,
-        dy: float,
+        self, result_x: NDArray[np.float64], result_y: NDArray[np.float64]
     ) -> None:
+        dx, dy, _ = self.cfg.scaled_grid_steps
+        v_x, v_y = self._v_x, self._v_y
         inv_2dx = 1.0 / (2.0 * dx)
         inv_2dy = 1.0 / (2.0 * dy)
 
