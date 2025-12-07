55ede672b89c3e133bb895cfaed499e45a596726
SlightlyMadMax <makcandreev2001@mail.ru>
2025-12-07 15:31:10 +0300

    update stand_with_icicle.py

--- src/utils/stand_with_icicle.py
+++ src/utils/stand_with_icicle.py
@@ -46,37 +46,39 @@ def init_temperature_icicle(
     # Combined mask
     mask = rect_mask | semicircle_mask
 
-    try:
-        x_step = float(X[0, 1] - X[0, 0]) if geometry.n_x > 1 else 1.0
-        y_step = float(Y[1, 0] - Y[0, 0]) if geometry.n_y > 1 else 1.0
-        dist = edt(mask, sampling=(y_step, x_step))
-    except Exception:
-        outside_idxs = np.column_stack(np.nonzero(~mask))
+    u[mask] = solid_temp
 
-        if outside_idxs.size == 0:
-            dist = np.zeros_like(X)
-            dist[mask] = 1.0
-        else:
-            xy_out = np.column_stack((X[~mask].ravel(), Y[~mask].ravel()))
-            xy_in = np.column_stack((X[mask].ravel(), Y[mask].ravel()))
-
-            d2 = (xy_in[:, None, 0] - xy_out[None, :, 0]) ** 2 + (
-                xy_in[:, None, 1] - xy_out[None, :, 1]
-            ) ** 2
-            min_dist_in = np.sqrt(d2.min(axis=1))
-
-            dist = np.zeros_like(X, dtype=float)
-            dist[mask] = min_dist_in
-
-    max_dist = float(dist[mask].max()) if np.any(mask) else 0.0
-    surface_temp = 273.15
-
-    if max_dist <= 0:
-        u[mask] = solid_temp
-    else:
-        normalized = np.zeros_like(dist, dtype=float)
-        normalized[mask] = dist[mask] / max_dist
-        u[mask] = surface_temp + (solid_temp - surface_temp) * normalized[mask]
+    # try:
+    #     x_step = float(X[0, 1] - X[0, 0]) if geometry.n_x > 1 else 1.0
+    #     y_step = float(Y[1, 0] - Y[0, 0]) if geometry.n_y > 1 else 1.0
+    #     dist = edt(mask, sampling=(y_step, x_step))
+    # except Exception:
+    #     outside_idxs = np.column_stack(np.nonzero(~mask))
+    #
+    #     if outside_idxs.size == 0:
+    #         dist = np.zeros_like(X)
+    #         dist[mask] = 1.0
+    #     else:
+    #         xy_out = np.column_stack((X[~mask].ravel(), Y[~mask].ravel()))
+    #         xy_in = np.column_stack((X[mask].ravel(), Y[mask].ravel()))
+    #
+    #         d2 = (xy_in[:, None, 0] - xy_out[None, :, 0]) ** 2 + (
+    #             xy_in[:, None, 1] - xy_out[None, :, 1]
+    #         ) ** 2
+    #         min_dist_in = np.sqrt(d2.min(axis=1))
+    #
+    #         dist = np.zeros_like(X, dtype=float)
+    #         dist[mask] = min_dist_in
+    #
+    # max_dist = float(dist[mask].max()) if np.any(mask) else 0.0
+    # surface_temp = 273.15
+    #
+    # if max_dist <= 0:
+    #     u[mask] = solid_temp
+    # else:
+    #     normalized = np.zeros_like(dist, dtype=float)
+    #     normalized[mask] = dist[mask] / max_dist
+    #     u[mask] = surface_temp + (solid_temp - surface_temp) * normalized[mask]
 
     u = (u - cfg.u_ref) / cfg.delta_u
 
