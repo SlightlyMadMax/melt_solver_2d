c3060480e1adac6595d713fd7dda4dddc6089715
SlightlyMadMax <makcandreev2001@mail.ru>
2025-12-08 17:47:05 +0300

    update plot_temperature_from_npz.py

--- src/utils/plot_temperature_from_npz.py
+++ src/utils/plot_temperature_from_npz.py
@@ -17,16 +17,16 @@ def plot_temperature_from_npz(
 ):
     exp_paths = sorted(
         glob.glob(files_path_mask),
-        key=lambda f: int(re.search(r"u_(\d+)", f).group(1)),
+        key=lambda f: int(re.search(r"checkpoint_(\d+)", f).group(1)),
     )
+
     for file_path in exp_paths:
-        match = re.search(r"u_(\d+)\.npz", file_path)
+        match = re.search(r"checkpoint_(\d+)\.npz", file_path)
         n = int(match.group(1))
         u = np.load(file_path)["u"]
         plot_temperature(
             u=u * cfg.delta_u + cfg.u_ref,
             cfg=cfg,
-            time=n * cfg.geometry.dt,
             graph_id=n,
             plot_boundary=True,
             show_graph=False,
