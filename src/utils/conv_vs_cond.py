31814c65a5d821711c482580078116c070ae2ea4
Maxim <lmaokeklol@gmail.com>
2026-01-29 16:39:50 +0300

    update conv_vs_cond.py

--- src/utils/conv_vs_cond.py
+++ src/utils/conv_vs_cond.py
@@ -2,11 +2,12 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 b_conv = np.load("../../data/wavy_surface/convection_boundary.npz")["b"]
-stef_conv = np.load("../../data/wavy_surface/stefan_boundary.npz")["b"]
+b_stef = np.load("../../data/wavy_surface/stefan_boundary.npz")["b"]
 
 time = []
-for n in range(stef_conv.shape[0]):
+for n in range(b_stef.shape[0]):
     time.append(n)
 
-plt.plot(time, stef_conv)
+plt.plot(time, b_stef)
+plt.plot(time, b_conv[:1201])
 plt.show()
