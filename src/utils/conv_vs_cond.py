ca007eefd98f702e29b227ee21645deab2fd938a
SlightlyMadMax <makcandreev2001@mail.ru>
2026-02-06 22:48:23 +0300

    reformatting

--- src/utils/conv_vs_cond.py
+++ src/utils/conv_vs_cond.py
@@ -1,13 +1,14 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-b_conv = np.load("../../data/wavy_surface/boundary/freezing/convection_boundary.npz")["b"]
-b_stef = np.load("../../data/wavy_surface/boundary/freezing/stefan_boundary.npz")["b"]
+b_conv = np.load("../../data/wavy_surface/boundary/melting/convection_boundary.npz")["b"]
+b_stef = np.load("../../data/wavy_surface/boundary/melting/stefan_boundary.npz")["b"]
 
 time = []
 for n in range(b_stef.shape[0]):
     time.append(n)
 
-plt.plot(time, b_stef)
-plt.plot(time, b_conv[:1201])
+plt.plot(time, b_stef, label="Pure conduction")
+plt.plot(time, b_conv[:1201], label="Convection")
+plt.legend()
 plt.show()
